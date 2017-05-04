3510c35f6b313a6e3a0a425c2f2efda160f23bc5
Piotr Korowicki <Piotrkorowicki@gmail.com>
2017-05-04 14:33:36 +0200

    ver1

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  var timer;
+  var windowHeight = $(window).height();
+  var triggerHeight = windowHeight;
+  var images = $('.slide');
+  var index = 0;
+
+  images[index].classList.add('visible');
+
+  $(window).scroll(function() {
+
+    window.clearTimeout(timer);
+    timer = window.setTimeout(function() {
+      var y = $(window).scrollTop() + windowHeight;
+
+      if (y > triggerHeight) {
+        images[index].classList.add('slide');
+        images[index].classList.remove('visible');
+        index++;
+        if (index > images.length - 1) {
+          index = 0;
+        }
+        images[index].classList.add('visible')
+        images[index].classList.remove('slide');
+      }
+    }, 100);
+  });
+
+
+});
